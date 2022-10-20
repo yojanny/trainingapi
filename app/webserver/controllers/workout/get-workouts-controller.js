f8d2f2aa6efbc871656bea94399556d4c73c5ffc
yojanny <yojannyreynoso@gmail.com>
2022-10-20 13:56:44 +0200

    tutoria berto

--- app/webserver/controllers/workout/get-workouts-controller.js
+++ app/webserver/controllers/workout/get-workouts-controller.js
@@ -3,13 +3,17 @@
 const mysqlPool = require('../../../database/mysql-pool/mysql-pool');
 
 async function getWorkouts(req, res) {
-  const query = `SELECT name, id, image, muscle FROM ejercicio`;
+  //const query = `SELECT name, id, image, muscle FROM ejercicio`;
+  const query = `
+    SELECT ej.*, (SELECT COUNT(user_id) FROM favorito WHERE ejercicio_id = ej.id AND user_id=?) AS fav
+    FROM ejercicio ej   
+    `;
 
   let connection = null;
   try {
     connection = await mysqlPool.getConnection();
 
-    const [workoutsList] = await connection.execute(query);
+    const [workoutsList] = await connection.execute(query, [req.claims.userId]);
     connection.release();
 
     return res.status(200).send({ status: 'ok', data: workoutsList });
